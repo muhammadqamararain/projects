35dc7d6fb2bdf187afcc310e1f670cdacd29ba79
muhammadqamararain <50930068+muhammadqamararain@users.noreply.github.com>
2019-05-24 16:46:47 +0500

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,4 @@
+pswrd=123
+user=int(input("please enter you 3 digit password:"))
+if user==pswrd:print("correct password, you are welcome:")
+else:print("password provided wrong, re-enter your password")
